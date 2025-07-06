b6345ece809faab9b620cbd89f85e4027b3d6177
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2025-07-07 00:15:11 +0100

    create image avatar component

---
+++ src/Lib/components/ImageAvatar.jsx
@@ -0,0 +1,20 @@
+
+import Image from "next/image"
+
+
+export default function ImageAvatar({url,size,orientation=""}){
+
+    function positionImage(){
+        switch(orientation.toLowerCase()){
+            case "center": return "items-center"
+            case "end": return "items-end"
+            default : return "items-start"
+        }
+    }
+
+    return (
+                <div style={{width:`${size}px`,height:`${size}px`}} className={` relative bg-red-900 overflow-hidden rounded-full flex ${positionImage()} `} >
+                    <Image className="rounded-full"  alt="Your Profile image" src={url} objectFit="cover"  fill />
+                </div>
+            )
+}
--- src/Lib/components/user.jsx
+++ src/Lib/components/user.jsx
@@ -1,14 +1,12 @@
 
 'use client'
-import Image from "next/image"
+import ImageAvatar from "./ImageAvatar";
 
-export default function User({name = "Daniel Ukuhor",position = "Software Engineer",imageSrc="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fHww"}){
+export default function User({name = "Daniel Ukuhor",position = "Software Engineer",imageSrc="https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHJhbmRvbSUyMHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D"}){
 
     return (
               <div className="flex">
-                <div className="w-[3rem] h-[3rem] overflow-hidden rounded-full flex items-center justify-center" >
-                  <Image className="rounded-full"  alt="Your Profile image" src={imageSrc}  width={48} height={48}/>
-                </div>
+                <ImageAvatar url={imageSrc} size={48}  />
                 <div>
                     <p>{name}</p>
                     <p>{position}</p>
